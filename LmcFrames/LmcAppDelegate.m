05c1a2cd9dcd01a251a5b2800b0528633b7e0320
MrBin <MrBin@mrbindemacbook-pro.local>
2016-05-24 16:25:33 +0800

    同步

--- LmcFrames/LmcAppDelegate.m
+++ LmcFrames/LmcAppDelegate.m
@@ -23,9 +23,6 @@
     //要判断，但发现我加上之后通过标签启动时不会加载预览界面
 //    if (launchOptions[@"UIApplicationLaunchOptionsShortcutItemKey"] == nil)
 //    {
-    
-    
-    
         return YES;
 //    }
 //    else
